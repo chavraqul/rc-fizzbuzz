c3027b0f3a945f186e154891a0abb3a612d08783
chavraqul <raquel.chavez@hp.com>
2015-09-14 21:47:37 -0500

    refactoring fizzbuzz to look a lot nicer and cleaner

--- js/app.js
+++ js/app.js
@@ -1,28 +1,49 @@
 $(document).ready(function() {
 
+
 	$('#btn').click(function(){
 		console.log("click listener");
 		$('.text').html('');
-		var num = document.getElementById("number").value;
-		var counter = 1;
+		
+		//var num = document.getElementById("number").value;
+
+		var num = $("#number").val();
+
 		parseInt(num);
 
-		while(counter <= num){
-			console.log('while loop');
-			
-			if((counter % 3 === 0) && (counter % 5 === 0)){
-				$('.text').append('<p>FizzBuzz</p>');
-			} else if(counter % 5 === 0){
-				$('.text').append('<p>Buzz</p>');
-			} else if(counter % 3 === 0){
-				$('.text').append('<p>Fizz</p>');
-			}else{
-				$('.text').append('<p>'+counter+'</p>');
-			} //end if else
-
-			counter++;
-		} //end while loop
+		if(num % 1 != 0 || num < 1){
+			alert('Please enter a pretty number that has no decimals and is greater than 0. Make this program happy :)');
+		}else{
+			calculate(num);
+		}
 
 	}) //end of .btn click listener
 	
 });
+
+function calculate(max){
+	var counter = 1;
+
+	while(counter <= max){
+			
+			if((counter % 3 == 0) && (counter % 5 == 0)){
+				//$('.text').append('<p>FizzBuzz</p>');
+				append('FizzBuzz');
+			} else if(counter % 5 == 0){
+				//$('.text').append('<p>Buzz</p>');
+				append('Buzz');
+			} else if(counter % 3 == 0){
+				//$('.text').append('<p>Fizz</p>');
+				append('Fizz');
+			}else{
+				//$('.text').append('<p>'+counter+'</p>');
+				append(counter);
+			} //end if else
+
+			counter++;
+		} //end while loop
+};
+
+function append(string){
+	return $('.text').append('<p>'+string+'</p>');
+}
